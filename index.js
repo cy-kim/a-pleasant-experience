4015b4b4ee5cb01b15c618c4dbf3e8f8c70bbd8f
Cy Kim <kimseongyeun@gmail.com>
2023-05-07 21:37:22 -0400

    cors()

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var http = require("http");
 let server, io;
 
 const PORT = process.env.PORT || 3000;
-app.use(cors);
+app.use(cors());
 app.use(express.static(path.join(__dirname, "/app")));
 
 server = http.Server(app);
